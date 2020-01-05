303cf58e2d6a399b223b1e6be957c7c69c080a17
Nguyen Vu Linh <nguyenvulinh99@gmail.com>
2020-01-05 15:27:02 +0700

    Linh: Add report QuanLyHocSinh

---
+++ DoAnLTUDQL1/ViewModels/QuestionStatisticViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoAnLTUDQL1.ViewModels
+{
+    public class QuestionStatisticViewModel
+    {
+        public int QuestionId { get; set; }
+        public int NumOfCorrect { get; set; }
+        public int NumOfWrong { get; set; }
+        
+    }
+}
---
+++ DoAnLTUDQL1/ViewModels/StudentStatisticViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoAnLTUDQL1.ViewModels
+{
+    public class StudentStatisticViewModel
+    {
+        public string ExamId { get; set; }
+        public string ExamDetailId { get; set; }
+        public string ExamName { get; set; }
+        public string StudentId { get; set; }
+        public string StudentName { get; set; }
+        public double Mark { get; set; }
+
+    }
+}
